a018e4cc39d06d9551008e4a7377f848a8dcdf62
Eli Burch <eli.burch@burchbytes.com>
2024-11-22 09:05:41 -0800

    Use non-static linking for now

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -14,6 +14,7 @@ wget "https://github.com/podofo/podofo/archive/refs/tags/0.10.4.zip" --output-do
 # Install podofo
 pushd "/tmp/podofo/podofo-0.10.4"
 mkdir build && cd build
-cmake -DPODOFO_BUILD_STATIC=TRUE ..
+# cmake -DPODOFO_BUILD_STATIC=TRUE ..
+cmake ..
 make
 sudo make install
