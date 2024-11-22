a6f403e07624692b290484a11086d5610ac7409d
Eli Burch <eli.burch@burchbytes.com>
2024-11-22 09:11:02 -0800

    Added some basic logging

--- pdfmerge.cpp
+++ pdfmerge.cpp
@@ -20,6 +20,7 @@ PdfMerge::PdfMerge(int argc, char ** argv)
 
     for (int i = 1; i < argc; i++) {
         if (strncmp(argv[i]+(strlen(argv[i])-4), ".pdf", 4) == 0){
+            printf("Adding %s to list\n", argv[i]);
             ui->pdfList->addItem(argv[i]);
         } else {
             printf("Argument %s is not a pdf, ignoring\n", argv[i]);
@@ -30,6 +31,7 @@ PdfMerge::PdfMerge(int argc, char ** argv)
 void PdfMerge::showError (char * msg) {
     char error[500];
     snprintf(error, 500, "Error: %s", msg);
+    printf("%s\n", error);
     ui->message->setText(error);
     ui->errorMessage->show();
 }
@@ -49,7 +51,7 @@ void PdfMerge::on_addFilesButton_clicked()
 void PdfMerge::on_mergeButton_clicked()
 {
     char * mergedFileName = ui->mergeFileName->text().toLatin1().data();
-
+    printf("Merging files ");
     if (std::filesystem::exists(mergedFileName)) {
         char msg[500];
         snprintf(msg, 500, "%s already exists.", mergedFileName);
@@ -61,10 +63,12 @@ void PdfMerge::on_mergeButton_clicked()
 
         for(int i = 0; i < count; i++){
             const QListWidgetItem * item = ui->pdfList->item(i);
+            printf("%s ", item->text().toLatin1().data());
             PoDoFo::PdfMemDocument doc;
             doc.Load(item->text().toLatin1().data());
             mergedDoc.GetPages().AppendDocumentPages(doc);
         }
+        printf("\n Saving to %s\n", mergedFileName);
 
         mergedDoc.Save(mergedFileName);
     }
