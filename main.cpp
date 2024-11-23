81ea428b45bf3504dca20f872cf8d03b80748eab
Eli Burch <eli.burch@burchbytes.com>
2024-11-23 11:47:19 -0800

    Log to log.txt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include "pdfmerge.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <QApplication>
+
+QtMessageHandler originalHandler = nullptr;
+
+void logToFile(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QString message = qFormatLogMessage(type, context, msg);
+    static FILE *f = fopen("log.txt", "a");
+    fprintf(f, "%s\n", qPrintable(message));
+    fflush(f);
+
+    if (originalHandler)
+        originalHandler(type, context, msg);
+}
 
 int main(int argc, char *argv[])
 {
+    originalHandler = qInstallMessageHandler(logToFile);
     QApplication a(argc, argv);
     PdfMerge w(argc, argv);
     w.show();
