4274ae38e10c54adca1924ac8cea376bd9be8aad
Eli Burch <eli.burch@burchbytes.com>
2024-11-23 11:37:14 -0800

    Replace printf with qInfo

--- pdfmerge.cpp
+++ pdfmerge.cpp
@@ -20,10 +20,10 @@ PdfMerge::PdfMerge(int argc, char ** argv)
 
     for (int i = 1; i < argc; i++) {
         if (strncmp(argv[i]+(strlen(argv[i])-4), ".pdf", 4) == 0){
-            printf("Adding %s to list\n", argv[i]);
+            qInfo("Adding %s to list\n", argv[i]);
             ui->pdfList->addItem(argv[i]);
         } else {
-            printf("Argument %s is not a pdf, ignoring\n", argv[i]);
+            qInfo("Argument %s is not a pdf, ignoring\n", argv[i]);
         }
     }
 }
@@ -31,7 +31,7 @@ PdfMerge::PdfMerge(int argc, char ** argv)
 void PdfMerge::showError (char * msg) {
     char error[500];
     snprintf(error, 500, "Error: %s", msg);
-    printf("%s\n", error);
+    qInfo("%s\n",error);
     ui->message->setText(error);
     ui->errorMessage->show();
 }
@@ -51,7 +51,7 @@ void PdfMerge::on_addFilesButton_clicked()
 void PdfMerge::on_mergeButton_clicked()
 {
     char * mergedFileName = ui->mergeFileName->text().toLatin1().data();
-    printf("Merging files ");
+    qInfo("Merging files ");
     if (std::filesystem::exists(mergedFileName)) {
         char msg[500];
         snprintf(msg, 500, "%s already exists.", mergedFileName);
@@ -63,12 +63,12 @@ void PdfMerge::on_mergeButton_clicked()
 
         for(int i = 0; i < count; i++){
             const QListWidgetItem * item = ui->pdfList->item(i);
-            printf("%s ", item->text().toLatin1().data());
+            qInfo("%s ", item->text().toLatin1().data());
             PoDoFo::PdfMemDocument doc;
             doc.Load(item->text().toLatin1().data());
             mergedDoc.GetPages().AppendDocumentPages(doc);
         }
-        printf("\n Saving to %s\n", mergedFileName);
+        qInfo("Saving to %s\n", mergedFileName);
 
         mergedDoc.Save(mergedFileName);
     }
