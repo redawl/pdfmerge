92a77787ca98a23625ddeddb7016092321ef17cd
Eli Burch <eli.burch@burchbytes.com>
2024-11-25 11:25:04 -0800

    Readd podofo as submodule

--- scripts/ci.ps1
+++ scripts/ci.ps1
@@ -1,16 +1,16 @@
-# create temp with zip extension (or Expand will complain)
-$tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } –PassThru
-# download
-Invoke-WebRequest -OutFile $tmp https://github.com/podofo/podofo/archive/refs/tags/0.10.4.zip
-# extract to same folder
-$tmp | Expand-Archive -DestinationPath podofo -Force
-# remove temporary file
-$tmp | Remove-Item
-cd podofo/podofo*
+## create temp with zip extension (or Expand will complain)
+#$tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } –PassThru
+## download
+#Invoke-WebRequest -OutFile $tmp https://github.com/podofo/podofo/archive/refs/tags/0.10.4.zip
+## extract to same folder
+#$tmp | Expand-Archive -DestinationPath podofo -Force
+## remove temporary file
+#$tmp | Remove-Item
+#cd podofo/podofo*
 vcpkg install pkgconf fontconfig freetype libxml2 openssl libjpeg-turbo libpng tiff zlib
-mkdir build
-cd build
-# cmake -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake -DPODOFO_BUILD_STATIC=TRUE ..
-cmake -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ..
-cmake --build . --config Release
-cmake --install .
+#mkdir build
+#cd build
+## cmake -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake -DPODOFO_BUILD_STATIC=TRUE ..
+#cmake -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+#cmake --build . --config Release
+#cmake --install .
--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 set -e
-
-# install qt6
-sudo apt update && sudo apt install -y libgl1-mesa-dev qt6-base-dev
-
 # install podofo dependencies
 sudo apt-get install -y libfontconfig1-dev libfreetype-dev libxml2-dev libssl-dev libjpeg-dev libpng-dev libtiff-dev libidn11-dev
 
-# Download podofo
-wget "https://github.com/podofo/podofo/archive/refs/tags/0.10.4.zip" --output-document /tmp/podofo.zip && unzip /tmp/podofo.zip -d /tmp/podofo
+## Download podofo
+#wget "https://github.com/podofo/podofo/archive/refs/tags/0.10.4.zip" --output-document /tmp/podofo.zip && unzip /tmp/podofo.zip -d /tmp/podofo
 
-# Install podofo
-pushd "/tmp/podofo/podofo-0.10.4"
-mkdir build && cd build
-# cmake -DPODOFO_BUILD_STATIC=TRUE ..
-cmake ..
-make
-sudo make install
+## Install podofo
+#pushd "/tmp/podofo/podofo-0.10.4"
+#mkdir build && cd build
+## cmake -DPODOFO_BUILD_STATIC=TRUE ..
+#cmake ..
+#make
+#sudo make install
